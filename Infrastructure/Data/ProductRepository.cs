091d433d0e16ce7adfc3eb08204a6ac8b5c7a2b5
marcinbalut <marcinbalutpoland@gmail.com>
2026-06-30 16:51:02 +0200

    Added brands and types to IProductRepository

--- Infrastructure/Data/ProductRepository.cs
+++ Infrastructure/Data/ProductRepository.cs
@@ -40,4 +40,18 @@ public class ProductRepository(StoreContext context) : IProductRepository
     {
         return context.Products.Any(x => x.Id == id);
     }
+
+    public async Task<IReadOnlyList<string>> GetBrandsAsync()
+    {
+        return await context.Products.Select(p => p.Brand)
+            .Distinct()
+            .ToListAsync();
+    }
+
+    public async Task<IReadOnlyList<string>> GetTypesAsync()
+    {
+        return await context.Products.Select(p => p.Type)
+            .Distinct()
+            .ToListAsync();
+    }
 }
