8e2c1da3c2d9c51cfd51e199037a03f0068eaa32
marcinbalut <marcinbalutpoland@gmail.com>
2026-06-30 17:08:24 +0200

    Added brands and types to ProductController

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -73,6 +73,18 @@ public class ProductsController(IProductRepository repo) : ControllerBase
 
         return BadRequest("Problem deleting the product");
     }
+
+    [HttpGet("brands")]
+    public async Task<ActionResult<IReadOnlyList<string>>> GetBrands()
+    {
+        return Ok(await repo.GetBrandsAsync());
+    }
+    
+    [HttpGet("types")]
+    public async Task<ActionResult<IReadOnlyList<string>>> GetTypes()
+    {
+        return Ok(await repo.GetTypesAsync());
+    }    
     
     private bool ProductExists(int id)
     {
