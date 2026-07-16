720a53f64856d4bfc908c7c9a0ae5b7dea95dcea
marcinbalut <marcinbalutpoland@gmail.com>
2026-07-16 16:50:40 +0200

    Added BaseSpecification<T, TResult> for projection

--- Core/Specifications/BaseSpecification.cs
+++ Core/Specifications/BaseSpecification.cs
@@ -22,3 +22,15 @@ public class BaseSpecification<T>(Expression<Func<T, bool>>? criteria) : ISpecif
         OrderByDescending = orderByDescExpression;
     }
 }
+
+public class BaseSpecification<T, TResult>(Expression<Func<T, bool>>? criteria)
+    : BaseSpecification<T>(criteria), ISpecification<T, TResult>
+{
+    protected BaseSpecification() : this(null) { }
+    public Expression<Func<T, TResult>>? Select { get; private set; }
+
+    protected void AddSelect(Expression<Func<T, TResult>> selectExpression)
+    {
+        Select = selectExpression;
+    }
+}
